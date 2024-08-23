9d26645d811f3e56bf186df40d23cb9ccd31c648
vince267 <110043082+vince267@users.noreply.github.com>
2024-08-22 21:02:43 -0400

    Initial attempt
    Wrote basic code skeleton to predict housing prices using random forest model. It's only trained on numeric features though!

--- predictions.py
+++ predictions.py
@@ -1,5 +1,41 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error 
 
-file_path = '/Users/Vince/Documents/Coding/Kaggle/Housing_Prices/home-data-for-ml-course/train.csv'
+# File path for training data and validation data
+train_file_path = '/Users/Vince/Documents/Coding/Kaggle/Kaggle-Housing-Prices/home-data-for-ml-course/train.csv'
+test_file_path = '/Users/Vince/Documents/Coding/Kaggle/Kaggle-Housing-Prices/home-data-for-ml-course/test.csv'
 
-training_data = pd.read_csv(file_path)
+# Store training and validation data
+training_data = pd.read_csv(train_file_path)
+test_data = pd.read_csv(test_file_path)
+
+# Store target column (sale price) as y 
+y = training_data.SalePrice
+
+# List of features we will focus on
+features = ['LotArea', 'OverallQual']
+
+# Store columns with features as X 
+X = training_data[features]
+
+# Store test data as test_X
+test_X = test_data[features]
+
+# Split X and y into train and validation data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify model
+rf_model = RandomForestRegressor(random_state=1)
+
+# Fit model to training data
+rf_model.fit(train_X, train_y)
+
+# # Make predictions about validation data
+predictions = rf_model.predict(val_X)
+
+# # Compare predictions with actual sale price
+MAE = mean_absolute_error(predictions, val_y)
+
+print(MAE)
